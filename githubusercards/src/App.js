81e6fa6bcc09864e5cc7cfe5f13fbac1d96aac9f
Chris Forrest <redrider_cr@yahoo.com>
2020-06-04 22:23:34 -0400

    Did first axios to see what data was returned

--- githubusercards/src/App.js
+++ githubusercards/src/App.js
@@ -6,7 +6,21 @@ import UserCard from './components/UserCard';
 import FollowersCard from './components/FollowersCard';
 
 class App extends React.Component {
+
+  state = {
+    user: [],
+  }
   
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/Chris-Forrest')
+        .then( res => {
+          console.log(res)
+        })
+        .catch( err => console.log('Fetch user error', err))
+  }
+
+
   render(){
     return(
       <div className='App'>
